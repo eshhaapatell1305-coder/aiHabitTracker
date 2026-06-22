729bce3071dde4373b57838472858bd1567cfdd4
eshhaapatell1305-coder <eshhaapatell1305@gmail.com>
2026-06-22 18:50:57 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,28 +1,17 @@
-import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import express from "express";
+import {
+    register,
+    login,
+    me,
+    updateProfile,
+} from "../controllers/authController.js";
+import { protect } from "../middleware/auth.js";
 
-export const protect = async (req, res, next)=>{
-    try{
-        let token;
-        if(
-            req.headers.authorization &&
-            req.headers.authorization.startsWith("Bearer")
-        ){
-            token = req.headers.authorization.split(" ")[1];
-        }
-        if(!token){
-            return res.status(401).json({ message: "Not authorized, no token"});
-        }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findById(decoded.id);
-        if(!user){
-            return res.status(401).json({ message:"User no longer exists"});
-        }
-        req.user = user;
-        next();
-    }catch(err){
-        return res
-        .status(401)
-        .json({message: "Not authorized, token invalid"});
-    }
-};
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", protect, me);
+router.put("/profile", protect, updateProfile);
+
+export default router;
